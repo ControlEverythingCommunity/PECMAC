2512099e05187ec356ee0283175f3f54a4869cd2
Yadwinder Singh <ryker1990@gmail.com>
2016-04-26 12:41:42 +0530

    Update PECMAC125A.ino

--- Particle/PECMAC125A.ino
+++ Particle/PECMAC125A.ino
@@ -14,7 +14,7 @@ unsigned int data[36];
 int typeOfSensor = 0;
 int maxCurrent = 0;
 int noOfChannel = 0;
-float current = 0.0;
+double current = 0.0;
 void setup()
 {
   // Set variable
@@ -67,7 +67,7 @@ void setup()
   maxCurrent = data[1];
   noOfChannel = data[2];
 
-  // Output data to serial monitor
+  // Output data to dashboard
   Particle.publish("Type Of Sensor : ", String(typeOfSensor));
   Particle.publish("Max Current : ", String(maxCurrent));
   Particle.publish("No. Of Channel : ", String(noOfChannel));
@@ -76,45 +76,45 @@ void setup()
 
 void loop()
 {
-  // Start I2C Transmission
-  Wire.beginTransmission(Addr);
-  // Command header
-  Wire.write(0x92);
-  // Command header
-  Wire.write(0x6A);
-  // Command 1
-  Wire.write(0x01);
-  // Start Channel No.
-  Wire.write(0x01);
-  // End Channel No.
-  Wire.write(noOfChannel);
-  // Reserved
-  Wire.write(0x00);
-  // Reserved
-  Wire.write(0x00);
-  // CheckSum
-  Wire.write((0x92 + 0x6A + 0x01 + 0x01 + noOfChannel + 0x00 + 0x00) & 0xFF);
-  // Stop I2C Transmission
-  Wire.endTransmission();
-  delay(300);
-
-  // Request (noOfChannel * 3) byte of data
-  Wire.requestFrom(Addr, noOfChannel * 3);
-
-  for (int j = 0; j < noOfChannel; j++)
+  for (int j = 1; j < noOfChannel + 1; j++)
   {
-    // Read 1 byte of data
+    // Start I2C Transmission
+    Wire.beginTransmission(Addr);
+    // Command header byte-1
+    Wire.write(0x92);
+    // Command header byte-2
+    Wire.write(0x6A);
+    // Command 1
+    Wire.write(0x01);
+    // Start Channel No.
+    Wire.write(j);
+    // End Channel No.
+    Wire.write(j);
+    // Reserved
+    Wire.write(0x00);
+    // Reserved
+    Wire.write(0x00);
+    // CheckSum
+    Wire.write((0x92 + 0x6A + 0x01 + j + j + 0x00 + 0x00) & 0xFF);
+    // Stop I2C Transmission
+    Wire.endTransmission();
+    delay(500);
+
+    // Request 3 bytes of data
+    Wire.requestFrom(Addr, 3);
+
+    // Read 3 bytes of data
     // msb1, msb, lsb
     int msb1 = Wire.read();
     int msb = Wire.read();
     int lsb = Wire.read();
-    float current = (msb1 * 65536) + (msb * 256) + lsb;
+    current = (msb1 * 65536) + (msb * 256) + lsb;
 
     // Convert the data to ampere
     current = current / 1000;
 
-    // Output to the serial monitor
-    Particle.publish("Channel : ", String(j + 1));
+    // Output data to dashboard
+    Particle.publish("Channel : ", String(j));
     delay(1000);
     Particle.publish("Current Value : ", String(current));
     delay(1000);
